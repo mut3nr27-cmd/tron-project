c1f04f4b9f3a961ff04be18dbefb8ef505990a2e
mut3nr27-cmd <mut3nr27@gmail.com>
2026-04-05 20:26:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask
 import os
+import ecdsa
+import base58
+import hashlib
 
-app = Flask(__name__)
+# توليد محفظة
+@app.route("/create-wallet")
+def create_wallet():
+    # توليد private key
+    private_key = os.urandom(32)
 
-@app.route('/')
-def home():
-    return {"status": "server running"}
+    # public key
+    sk = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
+    vk = sk.verifying_key
+    public_key = b'\x04' + vk.to_string()
 
-@app.route("/check-key")
-def check_key():
-    key = os.getenv("ADMIN_PRIVATE_KEY")
+    # hash
+    sha256 = hashlib.sha256(public_key).digest()
+    ripemd160 = hashlib.new('ripemd160', sha256).digest()
 
-    if key:
-        return {"status": "key loaded"}
-    else:
-        return {"status": "no key"}
+    address = b'\x41' + ripemd160
+    checksum = hashlib.sha256(hashlib.sha256(address).digest()).digest()[:4]
+    address_base58 = base58.b58encode(address + checksum)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    return {
+        "address": address_base58.decode(),
+        "private_key": private_key.hex()
+    }
