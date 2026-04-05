24a1210b590cca65dd3894558d9e451ff60df304
mut3nr27-cmd <mut3nr27@gmail.com>
2026-04-05 21:21:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,3 +34,28 @@ def create_wallet():
         "address": address_base58.decode(),
         "private_key": private_key.hex()
     }
+import requests
+
+@app.route("/build-usdt")
+def build_usdt():
+    owner = "PUT_YOUR_ADMIN_ADDRESS"   # 🔴 حط عنوانك (T...)
+    to = "PUT_RECEIVER_ADDRESS"        # 🔴 حط عنوان المستقبل
+
+    amount = 1_000_000  # 1 USDT (لأن USDT = 6 decimals)
+
+    payload = {
+        "owner_address": owner,
+        "contract_address": "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj",
+        "function_selector": "transfer(address,uint256)",
+        "parameter": "",  # رح نضبطه بالخطوة الجاية
+        "fee_limit": 100000000,
+        "call_value": 0,
+        "visible": True
+    }
+
+    r = requests.post(
+        "https://api.trongrid.io/wallet/triggersmartcontract",
+        json=payload
+    )
+
+    return r.json()
