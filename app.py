cd38027b0cadf12b56fe6245f3d847c6a589bd32
mut3nr27-cmd <mut3nr27@gmail.com>
2026-04-05 17:46:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, request, jsonify
+from tronpy.keys import PrivateKey
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Server running"
+    return "Server is running"
 
 @app.route("/register", methods=["POST"])
 def register():
     data = request.json
     username = data.get("username")
-    return jsonify({"message": f"User {username} registered"})
 
-@app.route("/send", methods=["POST"])
-def send():
-    data = request.json
-    msg = data.get("message")
-    return jsonify({"message": f"Sent: {msg}"})
+    private_key = PrivateKey.random()
+    address = private_key.public_key.to_base58check_address()
+
+    return jsonify({
+        "user": username,
+        "address": address,
+        "private_key": private_key.hex()
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=10000)
