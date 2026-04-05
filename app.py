a518dfbb56ab752a160a6a019297c46fa0985928
mut3nr27-cmd <mut3nr27@gmail.com>
2026-04-05 19:50:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 import os
+from tronpy import Tron
+from tronpy.keys import PrivateKey
 
 app = Flask(__name__)
+client = Tron()
 
+# الصفحة الرئيسية
 @app.route('/')
 def home():
-    return "Server running"
+    return {"status": "server running"}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-import os
-
+# فحص المفتاح
 @app.route("/check-key")
 def check_key():
     key = os.getenv("ADMIN_PRIVATE_KEY")
@@ -20,3 +20,17 @@ def check_key():
         return {"status": "key loaded"}
     else:
         return {"status": "no key"}
+
+# توليد محفظة
+@app.route("/create-wallet")
+def create_wallet():
+    wallet = client.generate_address()
+    return {
+        "address": wallet["base58check_address"],
+        "private_key": wallet["private_key"]
+    }
+
+# تشغيل السيرفر
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
