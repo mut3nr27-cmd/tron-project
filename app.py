415d939956e51043c9389cd86df6f28e499873d4
mut3nr27-cmd <mut3nr27@gmail.com>
2026-04-05 15:43:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Server is running 🚀"
 
-if name == "main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
